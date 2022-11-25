ac9e537d89e95e6dcbed72c94d1934e56bae9a7c
Amarnath Ganguli <amarnathsama@pop-os.localdomain>
2022-11-25 14:51:50 +0530

    added buttons, need to add channels to web socket

--- frontend/src/components/Channels/Channels.jsx
+++ frontend/src/components/Channels/Channels.jsx
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
-// import "./Channels.scss";
-import Message from "../Message/Message";
+import "./Channels.scss";
 import ChatHistory from "../ChatHistory/ChatHistory";
 import ChatInput from "../ChatInput/ChatInput";
+import { useEffect } from "react";
 
 function Channels(props) {
   const [currentPool, setCurrentPool] = useState(1);
+  const [channels, setChannels] = useState([]);
+
+  const handleAddChannel = () => {
+    setChannels((oldChannels) => {
+      const oldSize = oldChannels.length;
+
+      return [...oldChannels, oldSize + 1];
+    });
+  };
   return (
     <div className="Channels">
       <span>
         <h2>Channels</h2>
-        <button onClick={() => setCurrentPool(1)}>channel 1</button>
-        <button onClick={() => setCurrentPool(2)}>channel 2</button>
+        {channels.map((channels) => (
+          <button
+            className="button-9"
+            role="button"
+            onClick={() => setCurrentPool(channels)}
+          >
+            {`channel ${channels}`}
+          </button>
+        ))}
+        <button className="button-9" role="button" onClick={handleAddChannel}>
+          add channel
+        </button>
       </span>
       <ChatHistory ChatHistory={props.ChatHistory} poolID={currentPool} />
       <ChatInput poolID={currentPool} send={props.send}></ChatInput>
